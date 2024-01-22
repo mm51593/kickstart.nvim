29f9f5a6c5d02b1a30fcd83d64956c628bf44f87
Maksimilijan Marošević <mm51593@fer.hr>
2024-01-22 20:03:06 +0100

    Remap redo

--- lua/remaps/init.lua
+++ lua/remaps/init.lua
@@ -14,3 +14,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Sane redo
+vim.keymap.set('n', 'U', '<C-r>')
