8a1703d560a8d60c041f018168ce2fe08ed1c16b
Maksimilijan Marošević <maksimilijan.marosevic@proton.me>
2026-01-15 11:55:40 +0100

    Disable command history window

--- lua/remaps/init.lua
+++ lua/remaps/init.lua
@@ -5,6 +5,7 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<CR>', 'o<Esc>')
 vim.keymap.set('n', '<BS>', 'X')
+vim.keymap.set('n', 'q:', '<Nop>')
 
 -- Remap for dealing with word wrap
 -- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
